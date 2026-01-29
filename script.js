7aa0e758f4c3dfc16933859bbd3a32c721fc1978
Tom-8001 <mullertom2018@gmail.com>
2026-01-29 11:07:05 +0100

    Exo_3_Finie

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const div = document.querySelector('div');
-const position = document.querySelector('#position');
+const div = document.querySelector('body');
+const header = document.querySelector('header');
+const nivScroll = document.getElementById('nivScroll');
 
-//on récupère la position de la souris dans la div uniquement
-div.addEventListener('mousemove', () => {
-    console.log(`X: ${event.offsetX}, Y: ${event.offsetY}`);
-    position.textContent = `X: ${event.offsetX}, Y: ${event.offsetY}`;
+//on change la couleur de fond du header au scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+        header.style.backgroundColor = 'rgba(172, 15, 15, 0.8)';
+    } else {
+        header.style.backgroundColor = 'rgba(0, 0, 0)';
+    }
+});
+
+//on affiche en % en texte le niveau de scroll en pourcentage a nivScroll
+window.addEventListener('scroll', () => {
+    nivScroll.textContent = Math.round((window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100) + '%';
 });
